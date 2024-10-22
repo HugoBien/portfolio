9ab491df71ab5f6f6ec358ca270031e68ed6ba8d
HugoBien <hug.bien@gmail.com>
2024-10-22 21:40:12 +0200

    Ajout Skills.css + début page compétences

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -4,7 +4,22 @@ import './Skills.css'
 function Skills() {
 
     return (
-        <p>Skills</p>
+        <div>
+            <h1>Compétences</h1>
+            <div>
+                <h3>Programming language</h3>
+                <li>
+                    <img src="https://skillicons.dev/icons?i=py,java,c,cpp,cs,js,css,html,php"/>
+                </li>
+            </div>
+            <div>
+            <h3>Framework ?</h3>
+            <li>
+                <img src="https://skillicons.dev/icons?i=angular,react,vite,spring,nodejs,postgres,mysql"/>
+            </li>
+            </div>
+
+        </div>
     )
 }
 
