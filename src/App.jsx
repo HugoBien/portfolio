062527ee3ca80635fcfd8285e0aad080be550959
HugoBien <hugo.bienvenot9@etu.univ-lorraine.fr>
2024-10-24 09:17:52 +0200

    Début "a propos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-
 import './App.css'
+import telIcon from './assets/tel_icon.png';
+import mailIcon from './assets/mail_icon.png';
 
 
 function App() {
@@ -7,10 +8,28 @@ function App() {
 
   return (
 
-    <>
+      <div>
+        <div>
+          <h4>Je suis un étudiant en informatique</h4>
+        </div>
+        <div>
+          <h2>Hobby</h2>
+          <ul>
+            <li></li>
+          </ul>
+
+        </div>
+        <div>
+          <h2>Contact</h2>
+          <ul>
+            <li><img className="small-icon" src={telIcon}/>0657454842</li>
+            <li><img className="small-icon" src={mailIcon}/>hug.bien@gmail.com</li>
+          </ul>
+
+        </div>
 
 
-    </>
+      </div>
   )
 }
 
